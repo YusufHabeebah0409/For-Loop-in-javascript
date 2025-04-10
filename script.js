2f63749171750e7c111077bc4fe59afe35dd4eb5
YusufHabeebah0409 <ojuolapehabeebah@gmail.com>
2025-04-10 22:13:56 +0100

    Refactor arithmetic functions in script.js to improve structure and readability

--- script.js
+++ script.js
@@ -8,3 +8,32 @@ const addition = () => {
      
   }
 }
+const subtrate = () => {
+  for(i=1;i<=column.value;i++){
+    for(j=1;j<=row.value;j++){
+      showContent.innerHTML += ` <tr>
+            <td>${i} - ${j} = ${i-j}</td>
+        </tr>`
+    }
+     
+  }
+}
+const divide = () => {
+  for(i=1;i<=column.value;i++){
+    for(j=1;j<=row.value;j++){
+      showContent.innerHTML += ` <tr>
+            <td>${i} / ${j} = ${i/j}</td>
+        </tr>`
+    }
+  }
+}
+
+const multiple = () => {
+  for(i=1;i<=column.value;i++){
+    for(j=1;j<=row.value;j++){
+      showContent.innerHTML += ` <tr>
+            <td>${i} * ${j} = ${i*j}</td>
+        </tr>`
+    }
+  }
+}
