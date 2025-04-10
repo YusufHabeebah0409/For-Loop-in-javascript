6af0e42eb838c7385e492a168bc1d3f37434ca42
YusufHabeebah0409 <ojuolapehabeebah@gmail.com>
2025-04-10 22:05:54 +0100

    Add border to table in index.html and implement addition function in script.js

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-// for (let i = 1; i <= 100; i++) {
-//   if (i % 2 == 0) {
-//     show.innerHTML += `<h1 style="color:purple;">${i} Even Number</h1>`
-//   } else {
-//     show.innerHTML += `<h3 style="color:plum;">${i} Odd Number</h3>`
-//   }
-// }
-
-for (i=1;i<=5;i++) {
-  for (j=1;j<=12;j++) {
-    console.log(`${i} * ${j} = ${i*j}`);
-    
-  } 
+const addition = () => {
+  for(i=1;i<=column.value;i++){
+    for(j=1;j<=row.value;j++){
+      showContent.innerHTML += ` <tr>
+            <td>${i} + ${j} = ${i+j}</td>
+        </tr>`
+    }
+     
+  }
 }
