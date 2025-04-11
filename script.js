8c6e8b2ae2728f7c3e5b2ce1dad085b329c2e87d
YusufHabeebah0409 <ojuolapehabeebah@gmail.com>
2025-04-11 16:24:24 +0100

    Refactor arithmetic functions in script.js for improved structure and readability

--- script.js
+++ script.js
@@ -12,3 +12,52 @@ const addition = () => {
   column.value = ""
   row.value = ""
 }
+
+
+const subtrate = () => {
+ let output = showContent.innerHTML
+  output = ""
+  for (i = 1; i <= column.value; i++) {
+    output += `<tr>`
+    for (j = 1; j <= row.value; j++) {
+      output += `<td> ${i} - ${j} = ${i - j}</td>`
+    }
+    output += `</tr>`
+  }
+  showContent.innerHTML = output
+  column.value = ""
+  row.value = ""
+}
+
+
+const divide = () => {
+  let output = showContent.innerHTML
+   output = ""
+   for (i = 1; i <= column.value; i++) {
+     output += `<tr>`
+     for (j = 1; j <= row.value; j++) {
+       output += `<td> ${i} / ${j} = ${i / j}</td>`
+     }
+     output += `</tr>`
+   }
+   showContent.innerHTML = output
+   column.value = ""
+   row.value = ""
+ }
+
+
+
+ const multiple = () => {
+  let output = showContent.innerHTML
+   output = ""
+   for (i = 1; i <= column.value; i++) {
+     output += `<tr>`
+     for (j = 1; j <= row.value; j++) {
+       output += `<td> ${i} * ${j} = ${i * j}</td>`
+     }
+     output += `</tr>`
+   }
+   showContent.innerHTML = output
+   column.value = ""
+   row.value = ""
+ }
